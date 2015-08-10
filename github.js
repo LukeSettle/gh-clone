8d3cbf269643ba6fc7eb718123367585553964a1
Luke Settle <lukewestonsettle@gmail.com>
2015-08-10 17:21:49 -0500

    view profile does not work

--- github.js
+++ github.js
@@ -1,7 +1,15 @@
 $.getJSON("https://api.github.com/users/LukeSettle", function(data){
 	console.log(data);
+	$.getJSON(data.html_url)
 	$.getJSON(data.repos_url, repoCallback);
 	$.getJSON(data.followers_url, followersCallback)
+	var link = data.html_url
+	$(function(){
+		$(".view").on("click", function(){
+			$('.view').load(link);
+		});
+	});
+
 	$("<h1/>", {
 		text: data.name
 	}).appendTo("body");
@@ -9,11 +17,27 @@ $.getJSON("https://api.github.com/users/LukeSettle", function(data){
 	$("<h3/>", {
 		text: "location:" + data.location
 	}).appendTo("body");
-	
+
+	$("<h3/>", {
+		text: "Company:" + data.company
+	}).appendTo("body");
+
 	$.getJSON(data.avatar_url)
 	$("<img>", {
 		src: data.avatar_url
 	}).appendTo("body");
+
+	$.getJSON(data.bio)
+	if (data.bio != null){
+		$.getJSON(data.bio)
+		$("<h3/>", {
+			text: data.bio
+		}).appendTo("body");
+	} else {
+		$("<h3/>",{
+			text: "No bio"
+			}).appendTo("body");
+	};
 });
 
 var repoCallback = function(data){
@@ -31,7 +55,7 @@ var repoCallback = function(data){
 };
 
 var followersCallback = function(data){
-	var followersList = $("<ol/>");
+	var followersList = $("<ol/>").addClass("repos");
 	$("<h3/>", {
 		text: "Followers"
 	}).appendTo("body");
